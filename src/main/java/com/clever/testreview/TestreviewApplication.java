9abc95cc3969892e543b626818a6c5a3a82bb878
i.s.ivanov <56539727+is-ivanov@users.noreply.github.com>
2025-10-24 13:59:30 +0300

    Добавление файла TestreviewApplication.java

---
+++ src/main/java/com/clever/testreview/TestreviewApplication.java
@@ -0,0 +1,11 @@
+package com.clever.testreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestreviewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestreviewApplication.class, args);
+    }
+}
